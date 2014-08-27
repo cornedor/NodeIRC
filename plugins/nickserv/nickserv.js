f4d84ae35007d07d04b398e48f81fe8faff47296
Kevin van der Burgt <mail@kevinvdburgt.com>
2014-08-27 22:48:04 +0200

    Added the nickserv plugin

---
+++ plugins/nickserv/nickserv.js
@@ -0,0 +1,52 @@
+module.exports = function(bot, configuration) {
+    var client = bot.client,
+        plugin = bot.plugins,
+        config = bot.config,
+        name = bot.name;
+
+    var _nickservPassword = configuration.password || '';
+
+    /**
+     * Register all those given events which calls back the onEvent()
+     * @return {array} All events we need here
+     */
+    function events() {
+        // 'registered', 'motd','names','names#channel','topic',
+        // 'join','join#channel','part','part#channel','quit',
+        // 'kick','kick#channel','kill','message','message#',
+        // 'message#channel','notice','ping','pm','ctcp','ctcp-notice',
+        // 'ctcp-version','nick','invite','+mode','-mode','whois',
+        // 'channellist_start','channellist_item','channellist','raw',
+        // 'error'
+        return ['notice'];
+    };
+
+    /**
+     * This function is called when all plugins are ready to start.
+     */
+    function initialize() {
+        //console.log('This is an example plugin!');
+    };
+
+    /**
+     * Called event, defined in events();
+     * @param  {string} eventName The event name (see events();)
+     * @param  {array}  arguments All event arguments
+     */
+    function onEvent(eventName, arguments) {
+        if( eventName === 'notice' && 
+            arguments[2].substr(0, 27) === 'This nickname is registered' &&
+            arguments[3]['user'] === 'NickServ') {
+
+            console.log('LOGIN!');
+
+            client.say('nickserv', 'identify ' + _nickservPassword);
+        }
+    }
+
+    return {
+        events: events,
+        initialize: initialize,
+        onEvent: onEvent
+    }
+};
